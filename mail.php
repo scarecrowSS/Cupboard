85f150ffd4828620ecbf58b75d40991564085203
Artem <moonmh@mail.ru>
2022-07-07 13:23:48 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -5,13 +5,13 @@
     if (isset($_POST['phone'])) {$phone = $_POST['phone'];		if ($phone == '')	{unset($phone);}}
     //стирание треугольных скобок из полей формы
     // адрес почты куда придет письмо
-    $address="scarecrowsssssss@gmail.com";
+    $address="moonmh@mail.ru";
     //$address2="bayby_home@mail.ru ";
     // текст письма 
     $note_text="Тема : $theme \r\Телефон : $phone ";
     
     if (isset($phone)) {
     mail($address,$theme,$note_text,"Content-type:text/plain; windows-1251"); 
-    mail($address2,$theme,$note_text,"Content-type:text/plain; windows-1251");
+    //mail($address2,$theme,$note_text,"Content-type:text/plain; windows-1251");
     }
 ?>
